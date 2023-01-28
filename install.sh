2c73e1e0bb60172fab9ea37b9e7e96c01b436c2f
harunakts <harunakts@HarunNewUbuntu.myguest.virtualbox.org>
2023-01-29 00:58:23 +0300

    düzeltme

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ -f /mnt ];
 then
-cp -r data /mnt/data
+cp -r data /mnt/mysqlharundata
 helm install mysqlharun mysqlhelm/
 helm install harunaktasdocs docshelm/
 else
 mkdir /mnt
-cp -r data /mnt/data
+cp -r data /mnt/mysqlharundata
 helm install mysqlharun mysqlhelm/
 helm install harunaktasdocs docshelm/
 fi
