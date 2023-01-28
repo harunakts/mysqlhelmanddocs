3449a9e44f87892c63a7dfa90f8153f91961933a
root <root@HarunNewUbuntu.myguest.virtualbox.org>
2023-01-29 01:43:24 +0300

    mysql yetki sorunu cozuldu

--- install.sh
+++ install.sh
@@ -2,13 +2,11 @@
 if [ -f /mnt ];
 then
 cp -r data /mnt/mysqlharundata
-chown mysql:mysql -R /mnt/mysqlharundata
 helm install mysqlharun mysqlhelm/
 helm install harunaktasdocs docshelm/
 else
 mkdir /mnt
 cp -r data /mnt/mysqlharundata
-chown mysql:mysql -R /mnt/mysqlharundata
 helm install mysqlharun mysqlhelm/
 helm install harunaktasdocs docshelm/
 fi
